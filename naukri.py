a0a6c84fa7463d35cac31857fcd70536e7e249a2
surajsrj123 <patilsurajkmr@gmail.com>
2024-10-22 18:29:04 +0530

    this is prod code

--- naukri.py
+++ naukri.py
@@ -1,9 +1,9 @@
 
 
-# my_string= "my name is suraj"
-#
-# temp={i:my_string.count(i)for i in my_string}
-# print(temp)
+my_string= "my name is suraj"
+
+temp={i:my_string.count(i)for i in my_string}
+print(temp)
 
 my_dict= {"key":"value",1:"patil",True:"suraj",1:"priya"}
 
--- pra.py
+++ pra.py
@@ -58,16 +58,16 @@ print(a)
 
 
 
-# my_list = [1,23,5,4,2,18,16,49]
-#
-# for i in range(len(my_list)-1):
-#     print(i)
-#     for j in range(len(my_list)-1):
-#         if my_list[j]>my_list[j+1]:
-#
-#             my_list[j],my_list[j+1] = my_list[j+1],my_list[j]
-#
-# print(my_list[-1])
+my_list = [1,23,5,4,2,18,16,49]
+
+for i in range(len(my_list)-1):
+    print(i)
+    for j in range(len(my_list)-1):
+        if my_list[j]>my_list[j+1]:
+
+            my_list[j],my_list[j+1] = my_list[j+1],my_list[j]
+
+print(my_list[-1])
 
 
 # my_str = "sgdhgdjdgh"
